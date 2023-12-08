1a93ff1357427c8265eea3514c16365071820e3d
JosephIgbaji <igbajijoseph.k@gmail.com>
2023-12-08 17:06:17 +0100

    creating the login and registration page

--- src/app/order/page.jsx
+++ src/app/order/page.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="px-4">
+    <div className="h-screen flex flex-col justify-center items-center px-4">
       <h1>Kindly fill the form to place your order</h1>
+      <form action="">
+        <input type="text" />
+      </form>
     </div>
   );
 };
---
+++ src/app/register/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      Registration page in progress
+    </div>
+  );
+};
+
+export default page;
